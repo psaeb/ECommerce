561b0f23053cd5843e378eb51816acf8428f71cf
Pendar Dev <105170067+psaeb@users.noreply.github.com>
2023-01-17 23:09:16 -0500

    Add files via upload

---
+++ src/components/ui/Highlight.jsx
@@ -0,0 +1,11 @@
+function Highlight({ icon, title, description }) {
+  return (
+    <div className="highlight">
+      <div className="highlight__img">{icon}</div>
+      <h3 className="highlight__subtitle">{title}</h3>
+      <p className="highlight__para">{description}</p>
+    </div>
+  );
+}
+
+export default Highlight;
